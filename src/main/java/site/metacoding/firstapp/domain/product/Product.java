ade4eca72cb576ec39b9eca50092ede864e490af
s2staree <s2staree@gmail.com>
2022-09-30 15:09:43 +0900

    22-09-30 15:00 product DAO Junit 테스트 완료

--- src/main/java/site/metacoding/firstapp/domain/product/Product.java
+++ src/main/java/site/metacoding/firstapp/domain/product/Product.java
@@ -5,12 +5,32 @@ import java.sql.Timestamp;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
 public class Product {
     private Integer productId;
     private String productName;
-    private String productPrice;
+    private Integer productPrice;
     private Integer productQty;
     private Timestamp createdAt;
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    private Product() {
+
+    } // MyBatis에게 필요한 것
+
+    public Product(String productName, Integer productPrice, Integer productQty) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQty = productQty;
+    }
+
+    public void update(Product product) { // 수정할 수 있는 것만 넣기
+        this.productName = product.getProductName();
+        this.productPrice = product.getProductPrice();
+        this.productQty = product.getProductQty();
+    }
+
 }
--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -1,12 +1,22 @@
 package site.metacoding.firstapp.domain.product;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+import site.metacoding.firstapp.config.MyBatisConfig;
+
+@Import(MyBatisConfig.class) // MyBatisTest가 MyBatisConfig를 못읽음
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 실DB사용
+@MybatisTest
+// @SpringBootTest
 public class ProductDaoTest {
 
     @Autowired
@@ -15,12 +25,89 @@ public class ProductDaoTest {
     @Test
     public void findbyId_test() {
         // given (받아야 될 것)
-        Integer productId = 1;
+        Integer productId = 1; // Int써도 상관없음
 
         // when (테스트)
-        Product productPS = productDao.findById(productId);
+        Product productPS = productDao.findById(productId); // 영속화된 데이터이기때문에 PS붙이면 좋음
 
         // then (검증)
         assertEquals("바나나", productPS.getProductName());
+
+    }
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Product> productListPS = productDao.findAll();
+
+        // then
+        assertEquals(2, productListPS.size());
+    }
+
+    // Junit은 메서드 실행 직전에 트랜잭션이 걸리고, 메서드 실행이 끝나면 rollback 된다.
+    // MyBatis는 ResultSet을 자바 Entity로 변경해줄 때, 빈 생성자 + setter, 풀 생성자를 호출하고,
+    // setter가 없어도 값을 매핑해준다.
+    @Test
+    public void insert_test() {
+        // given
+        String productName = "수박";
+        Integer productPrice = 1000;
+        Integer productQty = 100;
+
+        Product product = new Product(productName, productPrice, productQty);
+
+        // when
+        int result = productDao.insert(product);
+
+        // then
+        assertEquals(1, result);
+
+    } // rollback
+
+    @Test
+    public void update_test() {
+        // given
+        Integer productId = 1;
+        String productName = "수박";
+        Integer productPrice = 1000;
+        Integer productQty = 100;
+
+        Product product = new Product(productName, productPrice, productQty);
+        product.setProductId(productId);
+
+        // verify (검증-순서관계없음)
+        Product productPS = productDao.findById(product.getProductId());
+        assertTrue(productPS == null ? false : true);
+
+        // when
+        // product [id=1, productName="수박", productPrice=1000, productQty=100]
+        // productPS [id=1, productName="바나나", productPrice=3000, productQty=98,
+        // createdAt=2022-09-29]
+        productPS.update(product);
+        // productPS [id=1, productName="수박", productPrice=1000, productQty=100,
+        // createdAt=2022-09-29]
+        int result = productDao.update(productPS);
+
+        // then (검증)
+        assertEquals(1, result);
+
+    }
+
+    @Test
+    public void deleteById_test() {
+        // given
+        Integer productId = 1;
+
+        // verify
+        Product productPS = productDao.findById(productId);
+        assertTrue(productPS == null ? false : true);
+
+        // when
+        int result = productDao.deleteById(productId);
+
+        // then
+        assertEquals(1, result);
     }
 }
